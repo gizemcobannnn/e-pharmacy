45d9c7dd57b25af67cfa230b7873bff9957c5abd
gizemcoban <gizem.coban.r@gmail.com>
2025-06-22 18:55:24 +0200

    login and register base

---
+++ pharmacyApp/src/pages/Register.jsx
@@ -0,0 +1,14 @@
+
+
+export default function Register() {
+  return (
+    <div className="w-full flex flex-row justify-around">
+        <div className="w-[50%]">
+            <p className="text-black text-start font-semibold text-5xl">Your medication, delivered Say goodbye to all <span className="text-green-500">your healthcare</span> worries with us</p>
+        </div>
+        <div className="w-[30%] grid grid-cols-2 gap-x-1 gap-y-1">
+            <input type="text" placeholder="Username" className="inputUser" /><input type="text" placeholder="Email" className="inputUser" /><input type="text" placeholder="Phone" className="inputUser" /><input type="text" placeholder="Password" className="inputUser"/>
+        </div>
+    </div>
+  )
+}
