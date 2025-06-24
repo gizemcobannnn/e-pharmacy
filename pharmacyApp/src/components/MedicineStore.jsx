b655810c77b64fcacb1c18682ae9b087397f0d12
gizemcoban <gizem.coban.r@gmail.com>
2025-06-24 20:03:31 +0200

    medicine store base

---
+++ pharmacyApp/src/components/MedicineStore.jsx
@@ -0,0 +1,21 @@
+export default function MedicineStore() {
+  return (
+    <>
+      <h2 className="text-2xl font-semibold">Medicine Store</h2>
+      <ul className="grid grid-cols-3 gap-x-2 gap-y-2 bg-white">
+        <li className="bg-[#4e7f49]">
+          <div className="flex flex-col">
+            <p className="font-semibold text-xl">name</p>
+            <p>konum</p>
+            <p>tel</p>
+            <div className="flex flex-row">
+              <button></button>
+              <p>yoldız</p>
+              <p>%</p>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </>
+  );
+}
