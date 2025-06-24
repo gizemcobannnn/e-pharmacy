bcbdbbc57c587cca950ba19736d27a55eed2f2bf
gizemcoban <gizem.coban.r@gmail.com>
2025-06-24 20:19:27 +0200

    details

---
+++ pharmacyApp/src/pages/MedicineDetails.jsx
@@ -0,0 +1,25 @@
+
+export default function MedicineDetails() {
+  return (
+    <>
+    <div>
+        <div>
+            <img src="" alt="" />
+            <div>
+                <p className="font-semibold text-xl">name</p>
+                <p className="font-semibold text-xl">fiyat</p>
+                <button>1</button>
+                <button>Add to cart</button>
+            </div>
+        </div>
+        <div className="flex flex-col gap-5">
+            <div>
+                <button>Description</button>
+                <button>Reviews</button>
+            </div>
+            <p>acıklama</p>
+        </div>
+    </div>
+    </>
+  )
+}
